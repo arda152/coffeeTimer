aafbac5e1b92dac1dc83e71a6d4f94225a12de77
arda152 <ardaarman99@hotmail.com>
2018-02-16 20:15:34 +0300

    Added progress bar
    Progress bar now shows Bloom/Brew/Done
    Added how to card

--- script.js
+++ script.js
@@ -1,41 +1,53 @@
-$(function() {
-    $("#submitButton").click(function(){
+$(function () {
+    $("#submitButton").click(function () {
+        if ($("#coffee").val() <= 0) {
+            alert("Please enter coffee amount!");
+            return false;
+        }
         let totalWater = Number($("#coffee").val() * (1000 / 60));
-        $("#display").html("You need " + parseInt($("#coffee").val() * 2) + " grams of water.");
+        $("#display").html(parseInt($("#coffee").val() * 2));
+        $("#progressbartag").html("Bloom");
+        $(".progress-bar").css("width", "12%");
         totalWater -= parseInt($("#coffee").val() * 2);
         let increment = totalWater / 120;
         console.log(increment);
         let i = 0;
         let j = 0;
-        let timer = setInterval(function(){
-            $("#timerDisplay").html(doublify(Math.floor(j / 60)) + ":" + doublify((j % 60)));
-            console.log(doublify(Math.floor(j / 60)) + ":" + doublify((j % 60)));
+        
+        // The bloom begins, water is 12 percent (two times the ground coffee) and timer begins counting
+        let timer = setInterval(function () {
+            //Start timer
+            $("#timerDisplay").html(doublify(Math.floor((j + 1) / 60)) + ":" + doublify(((j + 1) % 60)));
             j++;
-            if (j == 180)
-            {
+            if (j == 150) {
                 clearInterval(timer);
             }
         }, 1000);
-        setTimeout(function() {
-            let counter = setInterval(function(){
-            $("#display").html("You need " + parseInt(parseInt($("#coffee").val() * 2) + parseInt(increment * i)) + " grams of water.");
-            if (i == 120)
-            {
-                clearInterval(counter);
-            }
-            i++;
-        }, 1000);}, 30000);
+        
+        // The pour begins 30 seconds later
+        setTimeout(function () {
+            let counter = setInterval(function () {
+                $("#progressbartag").html("Brew");
+                $("#display").html(parseInt($("#coffee").val() * 2 + increment * i));
+                // Bloom is 12% of total water
+                // Map the value of i (0, 120) to percentage (12, 100)
+                $(".progress-bar").css("width", parseInt(12 + (i * (88 / 120))) + "%");
+                
+                // Stop brewing after two minutes of pour
+                if (i == 120) {
+                    $("#progressbartag").html("Done!");
+                    clearInterval(counter);
+                }
+                i++;
+            }, 1000);
+        }, 30000);
     });
 });
 
-function doublify(data)
-{
-    if (data < 10)
-        {
-            return "0" + data;
-        }
-    else
-        {
-            return data;
-        }
+function doublify(data) {
+    if (data < 10) {
+        return "0" + data;
+    } else {
+        return data;
+    }
 }
