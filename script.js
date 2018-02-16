3714a564125ee31d434d3f4343b8a0f4f5d2f191
arda152 <35403342+arda152@users.noreply.github.com>
2018-02-16 09:34:30 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+$(function() {
+    $("#submitButton").click(function(){
+        let totalWater = Number($("#coffee").val() * (1000 / 60));
+        $("#display").html("You need " + parseInt($("#coffee").val() * 2) + " grams of water.");
+        totalWater -= parseInt($("#coffee").val() * 2);
+        let increment = totalWater / 120;
+        console.log(increment);
+        let i = 0;
+        setTimeout(function() {
+            let counter = setInterval(function(){
+            $("#display").html("You need " + parseInt(parseInt($("#coffee").val() * 2) + parseInt(increment * i)) + " grams of water.");
+            if (i == 150)
+            {
+                clearInterval(counter);
+            }
+            i++;
+        }, 1000);}, 30000);
+    });
+});
+
